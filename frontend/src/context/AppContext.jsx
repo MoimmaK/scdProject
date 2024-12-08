f9fde7b8166feb21ddc84b89a755b1411cb90496
AhMaD TaShFeEn <i222490@nu.edu.pk>
2024-12-08 18:38:08 +0500

    App-Context Updated

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,5 +1,8 @@
 import { createContext, useState } from "react";
+import axios from "axios";
 import { doctors } from "../assets/assets";
+import { toast } from "react-toastify";
+import { set } from "mongoose";
 
 export const AppContext=createContext()
 
@@ -8,16 +11,31 @@ const AppContextProvider=(props)=>{
     const currencySymbol = '$'
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
-    const 
     const [token, setToken] = useState('')
 
+    const getDoctorsData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + '/api/doctor/list')
+            if (data.success){
+                setDoctors(data.doctors)
+            }
+            else{
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
 
+    
 
     const value = {
         doctors,
-        currencySymbol
+        currencySymbol,
+        token,setToken,
     }
-
     return(
         <AppContext.Provider value={value}>
         {props.children}

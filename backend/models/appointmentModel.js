a2d7a3003f9efaac8f65ceca264b7056bcb33640
Moimma Ali Khan <moimmak13@gmail.com>
2024-12-08 23:56:08 +0500

    added doctor profile with some missing functionality

--- backend/models/appointmentModel.js
+++ backend/models/appointmentModel.js
@@ -8,7 +8,7 @@ const appointmentSchema = new mongoose.Schema({
     userData: {type: Object, required:true},
     docData: {type: Object, required:true},
     amount: {type:Number, required:true},
-    date: {type: Boolean, default:false},
+    date: {type: Number, required:true},
     cancelled: {type:Boolean, default:false},
     payment: {type: Boolean, default:false},
     isCompleted: {type: Boolean, default:false}

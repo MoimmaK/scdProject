79667e211bdb41525c4e39d152c07f4343f2f815
AhMaD TaShFeEn <i222490@nu.edu.pk>
2024-12-08 20:54:17 +0500

    Patient Login and Registration updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,27 +2,28 @@ import validator from 'validator'
 import bcrypt from 'bcrypt'
 import userModel from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
+import {v2 as cloudinary} from 'cloudinary'
 
 
 //API to register user
 const registerUser = async (req, res) => {
     try {
-        const {name , email, password} = req.body
+        const { name, email, password } = req.body
 
-        if(!name || !email || !password) {
-            return res.status(400).json({success: false, message: "Please fill all fields"})
+        if (!name || !email || !password) {
+            return res.status(400).json({ success: false, message: "Please fill all fields" })
         }
 
 
         //validating email
-        if(!validator.isEmail(email)) {
-            return res.status(400).json({success: false, message: " Enter a Valid Email"})
+        if (!validator.isEmail(email)) {
+            return res.status(400).json({ success: false, message: " Enter a Valid Email" })
         }
 
 
         //validating password
-        if(password.length < 8) {
-            return res.status(400).json({success: false, message: "Password must be atleast 8 characters"})
+        if (password.length < 8) {
+            return res.status(400).json({ success: false, message: "Password must be atleast 8 characters" })
         }
 
         //hashing user password
@@ -38,49 +39,97 @@ const registerUser = async (req, res) => {
         const newUser = new userModel(userData)
         const user = await newUser.save()
         //_id
-        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET )
-        res.status(201).json({success: true, token})
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+        res.status(201).json({ success: true, token })
 
     } catch (error) {
         console.log(error)
-        res.json({success:false, message:error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
 //API for user login
 const loginUser = async (req, res) => {
     try {
-        const {email, password} = req.body
+        const { email, password } = req.body
 
-        if(!email || !password) {
-            return res.status(400).json({success: false, message: "Please fill all fields"})
+        if (!email || !password) {
+            return res.status(400).json({ success: false, message: "Please fill all fields" })
         }
 
         //validating email
-        if(!validator.isEmail(email)) {
-            return res.status(400).json({success: false, message: " Enter a Valid Email"})
+        if (!validator.isEmail(email)) {
+            return res.status(400).json({ success: false, message: " Enter a Valid Email" })
         }
 
-        const user = await userModel.findOne({email})
+        const user = await userModel.findOne({ email })
 
-        if(!user) {
-            return res.status(400).json({success: false, message: "User Does not Exist"})
+        if (!user) {
+            return res.status(400).json({ success: false, message: "User Does not Exist" })
         }
 
         const isMatch = await bcrypt.compare(password, user.password)
 
-        if(!isMatch) {
-            return res.status(400).json({success: false, message: "Invalid credentials"})
+        if (!isMatch) {
+            return res.status(400).json({ success: false, message: "Invalid credentials" })
         }
 
-        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET )
-        res.status(201).json({success: true, token})
-        
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+        res.status(201).json({ success: true, token })
+
     } catch (error) {
         console.log(error)
-        res.json({success:false, message:error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
+//API to get user details
+const getProfile = async (req, res) => {
+    try {
+        const { userId } = req.body
+        const userData = await userModel.findById(userId).select('-password')
 
-export {registerUser, loginUser}
+        res.status(200).json({ success: true, userData })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+
+    }
+}
+
+//API to update user Profile
+
+const updateProfile = async (req, res) => {
+    try {
+
+        const { userId, name, phone, address, dob, gender } = req.body
+        const imageFile = req.file
+        if (!name || !phone || !dob || !gender) {
+            return res.status(400).json({ success: false, message: "Please fill all fields" })
+        }
+
+        await userModel.findByIdAndUpdate(userId, {
+            name,
+            phone,
+            address: JSON.parse(address),
+            dob,
+            gender
+        })
+        if (imageFile){
+            //upload image to cloudinary
+            const imageUpload = await cloudinary.uploader.upload(imageFile.path,{resource_type:'image'})
+            const imageURL = imageUpload.secure_url
+
+            await userModel.findByIdAndUpdate(userId, {image:imageURL})
+        }
+
+        res.status(200).json({ success: true, message: "Profile Updated" })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export { registerUser, loginUser, getProfile, updateProfile}
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -9,10 +9,11 @@ const AppContextProvider = (props) => {
 
     const currencySymbol = '$'
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+
     const [doctors, setDoctors] = useState([])
-
     const[token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
-
+    const [userData, setUserData] = useState(false)
+    
     const getDoctorsData = async () => {
         try {
             const { data } = await axios.get(backendUrl + '/api/doctor/list')
@@ -27,16 +28,44 @@ const AppContextProvider = (props) => {
         }
     }
 
+    const loadUserProfileData = async () => {
+        try {
+
+            const {data}=await axios.get(backendUrl + '/api/user/get-profile', {headers:{token}})
+            if(data.success){
+                setUserData(data.userData)
+            }
+            else{
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error.message)
+            toast.error(error.message)
+        }
+    }
+
     const value = {
         doctors,
         currencySymbol,
         token, setToken,
-        backendUrl
+        backendUrl,
+        userData, setUserData,
+        loadUserProfileData
     }
 
     useEffect(()=> {
         getDoctorsData()
     }, [])
+
+    useEffect(() => {
+        if(token){
+            loadUserProfileData()
+        }
+        else{
+            setUserData(false)
+        }
+    },[token])
 
 
     return (

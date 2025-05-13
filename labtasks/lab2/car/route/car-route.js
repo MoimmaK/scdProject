2758c24be9ab0572464d83db086baa3ce4fbcd86
Zarmeena <125302733+notm33na@users.noreply.github.com>
2025-05-13 22:34:41 +0500

    Delete labtasks directory

--- labtasks/lab2/car/route/car-route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const carController = require('../controller/car-controller');
-
-router.post('/', carController.addCar);
-router.get('/:carId', carController.getCar);
-router.put('/:carId', carController.updateCarAvailability);
-
-module.exports = router;
--- labtasks/lab2/user/route/user-route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controller/user-controller');
-
-router.post('/', userController.registerUser);
-router.get('/:userId', userController.getUser);
-router.put('/:userId', userController.updateActiveBookings);
-
-module.exports = router;
